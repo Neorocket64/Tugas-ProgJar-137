98d940d5cd8986c09497c72141f8fc4e34b8fe15
Neorocket64 <neorocket64@gmail.com>
2020-03-02 13:01:30 +0700

    file edit and submission of tugas ke-2

---
+++ progjar2/udpfileserver.py
@@ -0,0 +1,23 @@
+import socket
+
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 5006
+NAMAFILE='coba.png'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+fp = open(NAMAFILE,'wb+')
+ditulis=0
+
+counter=0
+while True :
+    data, addr = sock.recvfrom(1024)
+    if not data :
+        break
+    counter=counter+len(data)
+    print(addr," blok ", counter,"panjang : ",len(data), data)
+    fp.write(data)
+print("success")
+fp.close()
