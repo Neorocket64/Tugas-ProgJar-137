18cc1d48a42f389a3b836b5dff49e0d054a01774
Neorocket64 <neorocket64@gmail.com>
2020-04-26 14:42:39 +0700

    tugas 8 done

--- tugas8/server.py
+++ tugas8/server.py
@@ -17,28 +17,17 @@ class ProcessTheClient(threading.Thread):
 
 	def run(self):
 		rcv=""
-		while True:
-			try:
-				data = self.connection.recv(32)
-				if data:
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						logging.warning("data dari client: {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						hasil=hasil+"\r\n\r\n"
-						logging.warning("balas ke  client: {}" . format(hasil))
-						self.connection.sendall(hasil.encode())
-						rcv=""
-						self.connection.close()
-				else:
-					break
-			except OSError as e:
-				pass
+		data = self.connection.recv(10000)
+		d = data.decode()
+		rcv=rcv+d
+		print(rcv)
+		logging.warning("data dari client: {}".format(rcv))
+		hasil = httpserver.proses(rcv)
+		hasil = hasil + "\r\n\r\n"
+		logging.warning("balas ke client: {}".format(hasil))
+		self.connection.sendall(hasil.encode())
 		self.connection.close()
-
-
+		print("close")
 
 class Server(threading.Thread):
 	def __init__(self):
@@ -48,7 +37,7 @@ class Server(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('127.0.0.1', 10001))
+		self.my_socket.bind(('127.0.0.1', 10002))
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
@@ -57,6 +46,7 @@ class Server(threading.Thread):
 			clt = ProcessTheClient(self.connection, self.client_address)
 			clt.start()
 			self.the_clients.append(clt)
+
 
 def main():
 	svr = Server()
